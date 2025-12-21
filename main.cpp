6871d2d9ffe68235ebe53b06c766c583d9783fef
Jidong Xiao <jidong.xiao@gmail.com>
2025-12-21 01:53:22 -0500

    make the project support CLI mode - now it compiles in Qt creator as well as in command line

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 #include <string>
 
 int run_cli();
+#ifdef CLI_ONLY
+int run_gui(int argc, char* argv[])
+{
+    std::cerr << "GUI not available in CLI build.\n";
+    return 1;
+}
+#else
 int run_gui(int argc, char* argv[]);
+#endif
 
 int main(int argc, char* argv[])
 {
